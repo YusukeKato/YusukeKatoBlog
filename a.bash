746ba2905addf65389f2c11548bb8d60c6262c5f
YusukeKato <takenoko2tomato2+github@gmail.com>
2023-04-08 21:51:14 +0900

    update

--- a.bash
+++ a.bash
@@ -1,4 +1,10 @@
 cp ./*.html ../
+cp ./*.gif ../
+cp ./*.png ../
+cp ./*.jpg ../
 cp ./style.css ../style.css
 echo "cp ./*.html ../"
+echo "cp ./*.gif ../"
+echo "cp ./*.png ../"
+echo "cp ./*.jpg ../"
 echo "cp ./style.css ../style.css"
