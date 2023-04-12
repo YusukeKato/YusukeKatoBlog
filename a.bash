461dd1a65db6431e57f569452e96e195d614bce0
YusukeKato <s1526035cn@s.chibakoudai.jp>
2023-04-12 21:16:05 +0900

    update sitemap.xml

--- a.bash
+++ a.bash
@@ -15,15 +15,18 @@ files2 = [filename for filename in files if 'html' in filename]
 f = open('sitemap.xml', 'w', encoding='UTF-8')
 f.write('<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n')
 f.write('<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n')
+f.write('\n')
 f.write('<url>\n')
 f.write('<loc>https://yusukekato.jp/</loc>\n')
 f.write('<priority>1.0</priority>\n')
 f.write('</url>\n')
+f.write('\n')
 for filename in files2:
   f.write('<url>\n')
-  f.write('<loc>https://yusukekato.jp/'+filename+'</loc>\n')
-  f.write('<priority>0.8</priority>\n')
+  f.write('  <loc>https://yusukekato.jp/'+filename+'</loc>\n')
+  f.write('  <priority>0.8</priority>\n')
   f.write('</url>\n')
+  f.write('\n')
 f.close()"
 echo "create sitemap.xml"
 
