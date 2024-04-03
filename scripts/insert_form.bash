d4e918a0be9417aed5e5637a92ac09cadcccba39
YusukeKato <takenoko2tomato2+github@gmail.com>
2024-04-03 21:37:40 +0900

    form埋め込み更新

--- scripts/insert_form.bash
+++ scripts/insert_form.bash
@@ -1,3 +1,3 @@
 a='<footer>'
-b='<iframe class=\"iframeClass\" src=\"https:\/\/docs.google.com\/forms\/d\/e\/1FAIpQLSefLzdebOIP_14ZVQLnyxOJZz8vPARVgNKyGaFbIY9Va7LG3Q\/viewform\?embedded=true\" width=\"640\" height=\"718\" frameborder=\"0\" marginheight=\"0\" marginwidth=\"0\">読み込んでいます…<\/iframe>\n\n<footer>'
+b='<div class=\"iframeClass\">\n<iframe class=\"formClass\" src=\"https:\/\/docs.google.com\/forms\/d\/e\/1FAIpQLSefLzdebOIP_14ZVQLnyxOJZz8vPARVgNKyGaFbIY9Va7LG3Q\/viewform\?embedded=true\" frameborder=\"0\" marginheight=\"0\" marginwidth=\"0\">読み込んでいます…<\/iframe>\n<\/div>\n\n<footer>'
 sed -i -e "s/$a/$b/" ../html/form.html
