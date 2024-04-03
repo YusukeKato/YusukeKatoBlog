3161d40446924ac45a06b12169eaa8bce9a77e6d
YusukeKato <takenoko2tomato2+github@gmail.com>
2024-04-03 21:16:38 +0900

    ブログ改造

--- README.md
+++ README.md
@@ -2,16 +2,13 @@
 https://yusukekato.jp/
 
 # update
-
 local PC
-
 ```sh
 cd scripts
 bash local_update.bash
 ```
 
 server
-
 ```sh
 cd scripts
 bash server_update.bash
---
+++ scripts/insert_form.bash
@@ -0,0 +1,3 @@
+a='<footer>'
+b='<iframe src=\"https:\/\/docs.google.com\/forms\/d\/e\/1FAIpQLSefLzdebOIP_14ZVQLnyxOJZz8vPARVgNKyGaFbIY9Va7LG3Q\/viewform\?embedded=true\" width=\"640\" height=\"718\" frameborder=\"0\" marginheight=\"0\" marginwidth=\"0\">読み込んでいます…<\/iframe>\n\n<footer>'
+sed -i -e "s/$a/$b/" ../html/form.html
