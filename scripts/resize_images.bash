1125659b2c3a8148943865f516eb5582803d0e14
YusukeKato <takenoko2tomato2+github@gmail.com>
2024-04-06 18:34:49 +0900

    resize_images.bashにdelete機能を追加

--- scripts/resize_images.bash
+++ scripts/resize_images.bash
@@ -1,3 +1,9 @@
 echo "start: resize images"
+
+# delete
+find ../images | grep "^../images/20.*/" | grep -e ".png.png$" -e ".jpg.png$" | xargs -I@ rm @
+
+# resize
 find ../images | grep "^../images/20.*/" | grep -e ".png$" -e ".jpg$" | xargs -I@ ffmpeg -y -i @ -vf scale=800:-1 @".png"
+
 echo "finish: resize images"
