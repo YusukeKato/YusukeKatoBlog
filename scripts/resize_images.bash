67130b1ca41b6b243ea21cde32dc204fa0cf74c7
YusukeKato <takenoko2tomato2+github@gmail.com>
2024-07-16 22:35:17 +0900

    画像のリサイズ時の削除対象を修正

--- scripts/resize_images.bash
+++ scripts/resize_images.bash
@@ -1,7 +1,7 @@
 echo "start: resize images"
 
 # delete
-find ../images | grep "^../images/20.*/" | grep -e ".png.png$" -e ".jpg.png$" | xargs -I@ rm @
+find ../images | grep "^../images/20.*/" | grep -e ".png.png$" -e ".jpg.png$" -e ".jpg.jpg$" -e ".png.jpg$" | xargs -I@ rm @
 
 # resize
 find ../images | grep "^../images/20.*/" | grep -e ".png$" -e ".jpg$" | xargs -I@ ffmpeg -y -i @ -vf scale=800:-1 @".jpg"
