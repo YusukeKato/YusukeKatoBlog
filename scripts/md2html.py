6353c7549eadf7a982d16d64dc59d2343b82e4af
YusukeKato <takenoko2tomato2+github@gmail.com>
2024-07-16 22:32:10 +0900

    れもんの日記4:7/16追加、画像のリサイズをjpgに変更

--- scripts/md2html.py
+++ scripts/md2html.py
@@ -165,7 +165,7 @@ for markdown_file in markdown_files:
                     i += 2
                 elif re.match('.+\.png', read_lines[i]) or re.match('.+\.jpg', read_lines[i]): # 画像を変換
                     write_lines.append('<div class="img">\n')
-                    write_lines.append('<img src="https://yusukekato.jp/images/' + read_lines[i] + '.png' + '" alt="' + read_lines[i+2] + '">\n')
+                    write_lines.append('<img src="https://yusukekato.jp/images/' + read_lines[i] + '.jpg' + '" alt="' + read_lines[i+2] + '">\n')
                     write_lines.append('</div>\n')
                     i += 2
                 elif re.match('.+\.gif', read_lines[i]): # GIF画像を変換
