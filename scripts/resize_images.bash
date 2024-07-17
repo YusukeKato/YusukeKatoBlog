3a7d7c72f6a6d1137c2ff7a483a8e10191b87ba9
YusukeKato <yusuke.kato@rt-net.jp>
2024-07-17 12:11:46 +0900

    リサイズ修正、pngではなくjpg

--- scripts/resize_images.bash
+++ scripts/resize_images.bash
@@ -4,7 +4,7 @@ echo "start: resize images"
 find ../images | grep "^../images/20.*/" | grep -e ".png.png$" -e ".jpg.png$" -e ".jpg.jpg$" -e ".png.jpg$" -e ".gif.gif$" | xargs -I@ rm @
 
 # resize
-find ../images | grep "^../images/20.*/" | grep -e ".png$" -e ".jpg$" | xargs -I@ ffmpeg -y -i @ -vf scale=640:-1 @".jpg"
+find ../images | grep "^../images/20.*/" | grep -e ".png$" -e ".jpg$" | xargs -I@ ffmpeg -y -i @ -vf scale=480:-1 @".jpg"
 find ../images | grep "^../images/20.*/" | grep -e ".gif$" | xargs -I@ ffmpeg -y -i @ -r 10 -vf scale=320:-1 @".gif"
 find ../images | grep "^../images/20.*/" | grep -e ".gif.gif$" | xargs -I@ convert @ -layers Optimize @
 
