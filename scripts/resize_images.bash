a2b78295e76d601909d5ca3bd01b133d58d1fba6
YusukeKato <takenoko2tomato2+github@gmail.com>
2024-07-17 22:09:59 +0900

    画像サイズを統一

--- scripts/resize_images.bash
+++ scripts/resize_images.bash
@@ -4,8 +4,8 @@ echo "start: resize images"
 find ../images | grep "^../images/20.*/" | grep -e ".png.png$" -e ".jpg.png$" -e ".jpg.jpg$" -e ".png.jpg$" -e ".gif.gif$" | xargs -I@ rm @
 
 # resize
-find ../images | grep "^../images/20.*/" | grep -e ".png$" -e ".jpg$" | xargs -I@ ffmpeg -y -i @ -vf scale=480:-1 @".jpg"
-find ../images | grep "^../images/20.*/" | grep -e ".gif$" | xargs -I@ ffmpeg -y -i @ -r 10 -vf scale=320:-1 @".gif"
+find ../images | grep "^../images/20.*/" | grep -e ".png$" -e ".jpg$" | xargs -I@ ffmpeg -y -i @ -vf scale=400:-1 @".jpg"
+find ../images | grep "^../images/20.*/" | grep -e ".gif$" | xargs -I@ ffmpeg -y -i @ -r 10 -vf scale=400:-1 @".gif"
 find ../images | grep "^../images/20.*/" | grep -e ".gif.gif$" | xargs -I@ convert @ -layers Optimize @
 
 echo "finish: resize images"
