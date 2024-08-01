efc9fc9bc458cc9183d827d24597c51fb77064df
YusukeKato <takenoko2tomato2+github@gmail.com>
2024-08-01 23:46:40 +0900

    8/1れもん日記を追加

--- scripts/resize_images.bash
+++ scripts/resize_images.bash
@@ -3,6 +3,9 @@ echo "start: resize images"
 # delete
 find ../images | grep "^../images/20.*/" | grep -e ".png.png$" -e ".jpg.png$" -e ".jpg.jpg$" -e ".png.jpg$" -e ".gif.gif$" | xargs -I@ rm @
 
+# rename
+find ../images | grep "^../images/20.*/" | grep -e ".jpeg$" | sed 's/.jpeg//g' | xargs -I@ mv @".jpeg" @".jpg"
+
 # resize
 find ../images | grep "^../images/20.*/" | grep -e ".png$" -e ".jpg$" | xargs -I@ ffmpeg -y -i @ -vf scale=600:-1 @".jpg"
 find ../images | grep "^../images/20.*/" | grep -e ".gif$" | xargs -I@ ffmpeg -y -i @ -r 10 -vf scale=400:-1 @".gif"
