81e4512f89e0633af2a923639adccd2845fc5b78
YusukeKato <yusukekato.contact@gmail.com>
2026-03-04 23:38:50 +0900

    デプロイ方法を更新

--- scripts/delete_html.bash
+++
@@ -1,5 +0,0 @@
-echo 'start: delete html'
-echo 'delete html list'
-find ../html | grep '\.html$'
-find ../html | grep '\.html$' | xargs -I@ rm @
-echo 'finish: delete html'
--- scripts/md2html.bash
+++
@@ -1,6 +0,0 @@
-# markdown to html
-if type 'python3' > /dev/null 2>&1; then
-	python3 md2html.py
-else
-	python md2html.py
-fi
